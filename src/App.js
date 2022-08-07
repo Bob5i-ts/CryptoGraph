304f4ce27d313fe3b375554fca3ab2aa01146d7a
Bob5i-ts <91635735+Bob5i-ts@users.noreply.github.com>
2022-08-07 13:40:57 +0300

    Add router and auth guards to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 import { AuthProvider, DatabaseProvider, useFirebaseApp } from 'reactfire';
+import { Switch, Route, BrowserRouter, Redirect} from 'react-router-dom';
+import { PrivateWrapper, PublicWrapper } from './components/AuthWrappers';
 import './css/App.css';
 
 function App() {
@@ -10,6 +12,12 @@ function App() {
   return (
     <AuthProvider sdk={auth}>
       <DatabaseProvider sdk={db}>
+        <BrowserRouter>
+          <PublicWrapper>
+          </PublicWrapper>
+          <PrivateWrapper>
+          </PrivateWrapper>
+        </BrowserRouter>
       </DatabaseProvider>
     </AuthProvider>
   );
---
+++ src/components/AuthWrappers.js
@@ -0,0 +1,20 @@
+import { useUser } from 'reactfire';
+
+export function PublicWrapper({ children }) {
+    const user = useUser();
+    if (!user.data) {
+        return children;
+    }
+
+    return null;
+}
+
+export function PrivateWrapper({ children }) {
+    const user = useUser();
+    if (user.data) {
+        return children;
+    }
+
+    return null;
+}
+
