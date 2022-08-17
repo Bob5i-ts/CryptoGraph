ec79c25fc81cb57683b817d4703d8b8019a0ecc0
Bob5i-ts <91635735+Bob5i-ts@users.noreply.github.com>
2022-08-18 00:16:54 +0300

    Move firebase config to new folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { FirebaseAppProvider } from 'reactfire';
-import { appConfig } from './services/firebaseAPI';
+import { appConfig } from './config/firebase';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import './css/index.css';
