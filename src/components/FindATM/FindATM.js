66018b6e34a807a70d3d8d661807bf85991290af
Bob5i-ts <91635735+Bob5i-ts@users.noreply.github.com>
2024-01-09 23:58:35 +0200

    Minor visual changes to Find ATM page

--- src/components/FindATM/FindATM.js
+++ src/components/FindATM/FindATM.js
@@ -1,41 +1,38 @@
 import MainWrapper from '../MainWrapper/MainWrapper';
 import { MainToolbar } from '../MainWrapper/Elements';
-import './FindATM.css';
 import { useEffect, useRef, useState } from 'react';
+import './FindATM.css';
 
 function FindATM() {
+    useEffect(() => document.title = 'Find ATM | CryptoGraph');
+
     const [query, setQuery] = useState('ATMs');
     const searchRef = useRef();
     const atmRadioRef = useRef();
     const btcAtmRadioRef = useRef();
-    useEffect(() => {
-        document.title = 'Find ATM | CryptoGraph';
-    });
+
     function searchAtms() {
         const atmType = atmRadioRef.current.checked
             ? atmRadioRef.current.value
             : btcAtmRadioRef.current.value;
-        setQuery(atmType +','+ searchRef.current.value)
+        setQuery(atmType + ',' + searchRef.current.value);
     }
     return (
         <MainWrapper>
             <MainToolbar>
                 <input type='search' id='atm-input' name='atm-search' placeholder='Location' ref={searchRef}/>
-
-                <input type='radio' id='atm-radio' name='atm-type' value='ATMs' defaultChecked ref={atmRadioRef}/>
-                <label htmlFor='atm-radio'>ATMs</label>
-
-                <input type='radio' id='btc-atm-radio' name='atm-type' value='BTC ATMs' ref={btcAtmRadioRef}/>
-                <label htmlFor='btc-atm-radio'>Crypto ATMs</label>
-
+                <div>
+                    <input type='radio' id='atm-radio' name='atm-type' value='ATMs' defaultChecked ref={atmRadioRef}/>
+                    <label htmlFor='atm-radio'>ATMs</label>
+                    <input type='radio' id='btc-atm-radio' name='atm-type' value='BTC+ATMs' ref={btcAtmRadioRef}/>
+                    <label htmlFor='btc-atm-radio'>Crypto ATMs</label>
+                </div>
                 <button onClick={searchAtms}>Search</button>
             </MainToolbar>
 
             <iframe className='map-container' loading='lazy' allowFullScreen
                 referrerPolicy='no-referrer-when-downgrade' title='g-map'
-                src={'https://www.google.com/maps/embed/v1/search?key=' + process.env.REACT_APP_MAPS_API_KEY
-                    + '&q=' + query
-                }
+                src={'https://www.google.com/maps/embed/v1/search?key=' + process.env.REACT_APP_MAPS_API_KEY + '&q=' + query}
             >
             </iframe>
         </MainWrapper>
